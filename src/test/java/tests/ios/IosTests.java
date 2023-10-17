eed1bd4b24cfd1d3ae5cffb945c6adc4eb2f8376
esilaeva <emsilaeva@gmail.com>
2023-10-17 15:26:52 +0300

    add ios test

--- src/test/java/tests/ios/IosTests.java
+++ src/test/java/tests/ios/IosTests.java
@@ -1,40 +1,38 @@
 package tests.ios;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
-import java.net.MalformedURLException;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IosTests extends TestBase {
 
     @Test
     @Tag("ios")
-    void successfulOpenArticle() throws MalformedURLException, InterruptedException {
+    @DisplayName("Проверка ввода и отображения текста в UI Elements")
+    void enterTextTest() {
 
-        step("Click on the tab Newest", () -> {
-            $(id("Newest")).click();
+        step("Нажать на кнопку Text", () -> {
+            $(id("Text Button")).click();
         });
 
-//        step("Open article", () -> {
-//            $(id("org.wikipedia.alpha:id/page_list_item_title")).click();
-//        });
+        step("Нажать на поле Enter a text", () -> {
+            $(id("Text Input")).click();
+        });
 
-        //
-        //Newest
-        //Tux in the Ring [video]
+        step("Ввести в поле Enter a text текст Hello QA.GURU и нажать Enter", () -> {
+            $(id("Text Input")).sendKeys("Hello QA.GURU");
+            $(id("Text Input")).pressEnter();
+        });
 
-
-        step("Verify content found", () -> {
-            $(byText("Tux in the Ring [video]")).shouldBe(visible);
+        step("Проверка отображения результата с заданным текстом", () -> {
+            assertThat($(id("Text Output")).getText())
+                    .isEqualTo("Hello QA.GURU");
         });
     }
 }
